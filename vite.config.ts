da8e83a1f941ac6ab8ea0c0147824e5f63d64404
ShadowUchiha432 <ShadowUchiha432@gmail.com>
2026-09-09 02:05:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,11 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages repository path
+  base: "/Happy-Birthday-Charmy/",
+
   plugins: [react(), tailwindcss(), viteSingleFile()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
